9d32a07e61b17d668470bdcea06af2c385d446c4
Mael <56530380+juanmijael-salazar@users.noreply.github.com>
2022-12-19 15:04:26 -0500

    cambios2

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include 'views/head.php'; ?>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
+    <link rel="stylesheet" href="assets/style.css?v=2">
+</head>
+<body>
+<?php include 'views/top.php'; ?>
+<div class="container">
+    Hola Juan Mijael Salazar Cutipa
+</div>
+
+<script src="assets/demo.js?v2"></script>
+</body>
+</html>
